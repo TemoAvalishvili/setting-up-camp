b6a40857240f90c8809205357eac47d03e3ef8d9
Temo Avalishvili <temo.avalishvili7@gmail.com>
2024-01-27 13:49:53 +0400

    Imrovised content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowIntro(false);
+    }, 0);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-container">
+      {showIntro && <GameIntro showIntro={showIntro} />}
+      <BuildTent />
     </div>
   );
 }
 
-export default App;
+function GameIntro({ showIntro }) {
+  const [isFaded, setIsFaded] = useState(false); // Separate state for fade effect
+
+  useEffect(() => {
+    // Update isFaded when showIntro changes
+    setIsFaded(!showIntro);
+  }, [showIntro]);
+
+  return (
+    <div className={`intro-container fade-in ${isFaded ? "show" : ""}`}>
+      <h1>Welcome to the game!</h1>
+      <h3>Enjoy your Journey!</h3>
+    </div>
+  );
+}
+
+function BuildTent() {
+  return (
+    <div className="campButton">
+      <p className="flickering-text">Click to build a tent!</p>
+      <img
+        src={require("./images/tent.png")}
+        className="tent"
+        alt="tent"
+        width="80"
+        height="80"
+      />
+    </div>
+  );
+}
